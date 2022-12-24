871ed2739017c7359725ad46e6fee02a9bb09200
Alex Tsaparas <up1072824@upnet.gr>
2022-12-24 02:37:13 +0200

    Update urls.py

--- Database_Management/urls.py
+++ Database_Management/urls.py
@@ -4,16 +4,23 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('supplier/', views.supplier, name='supplier'),
+    path('consists_of/', views.consistsOf, name='consistsOf'),
+    path('contract/', views.contract, name='contract'),
+    path('customers/', views.customer, name='customer'),
     path('employee/', views.employee, name='employee'),
     path('employee/<str:ssn>/', views.employee_delete),
-    path('contract/', views.contract, name='contract'),
-    path('signs/', views.signs, name='signs'),
-    path('services/', views.service, name='service'),
+    path('entails/', views.entails, name='entails'),
+    path('gas_stations/', views.gasStation, name='gasStation'),
+    path('involves/', views.involves, name='involves'),
     path('is_assigned_to/', views.isAssignedTo, name='isAssignedTo'),
+    path('offers/', views.offers, name='offers'),
     path('products/', views.product, name='product'),
-    path('tanks/', views.tank, name='tank'),
+    path('provides/', views.provides, name='provides'),
     path('pumps/', views.pump, name='pump'),
-    path('customers/', views.customer, name='customer'),
-    path('gas_stations/', views.gasStation, name='gasStation')
+    path('purchases/', views.purchase, name='purchase'),
+    path('services/', views.service, name='service'),
+    path('signs/', views.signs, name='signs'),
+    path('supplier/', views.supplier, name='supplier'),
+    path('supplies/', views.supply, name='supply'),
+    path('tanks/', views.tank, name='tank')
 ]

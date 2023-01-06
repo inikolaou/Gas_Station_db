f32625857bca18cb6afa94cbf2818fbb7f70ebae
Alexandros Tsaparas <alextsaparas@icloud.com>
2023-01-06 13:43:02 +0200

    Ficed date from 1 -> 01
    Αν είναι το τρέχουμε μια φορά για να αλλάξουν τα αρχεία και στο σβήνουμε μετά

--- Entities/Tank.py
+++ Entities/Tank.py
@@ -1,6 +1,29 @@
 import sqlite3
 import csv
+import pandas as pd
 
+df = pd.read_csv("Datasets/tank.csv")
+li = []
+for date in df['Last_Check_Up']:
+    numbers = date.split('/')
+    if (len(numbers[0]) == 1):
+        if (len(numbers[1]) == 1):
+            day = '0' + numbers[0]
+            month = '0' + numbers[1]
+        else:
+            day = '0' + numbers[0]
+            month = numbers[1]
+    else:
+        if (len(numbers[1]) == 1):
+            day = numbers[0]
+            month = '0' + numbers[1]
+        else:
+            day = numbers[0]
+            month = numbers[1]
+    correct_date = '/'.join([day, month, numbers[2]])
+    li.append(correct_date)
+df['Last_Check_Up'] = li
+df.to_csv("Datasets/tank.csv", index=False)
 
 def createTankTable():
     conn = sqlite3.connect("Gas_Station.db")
